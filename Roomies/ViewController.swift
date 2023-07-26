955a8da1e0c56b8d302118e70d19173584685c61
Emilija Chona <emmachona01@gmail.com>
2023-07-26 08:45:43 -0700

    Initial Commit

---
+++ Roomies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Roomies
+//
+//  Created by Emilija Chona on 7/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
